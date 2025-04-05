fea4aaef42d9ea69dd7c3d64bb5dd98da9b355f2
jay <97389656+zhamo026@users.noreply.github.com>
2025-04-05 07:04:49 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Website Loaded Successfully!");
-
-  const toggle = document.getElementById("projects-toggle");
-  const menu = document.getElementById("projects-menu");
-
-  toggle.addEventListener("click", function (e) {
-    e.preventDefault();
-    menu.classList.toggle("show");
-  });
-
-  // Optional: Close the submenu if clicking outside
-  document.addEventListener("click", function (e) {
-    if (!toggle.contains(e.target) && !menu.contains(e.target)) {
-      menu.classList.remove("show");
-    }
-  });
+    console.log("Website Loaded Successfully!");
 });
