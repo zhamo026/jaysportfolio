1d5a686d195d96f3d4ba48ec87219cb6d0871678
jay <97389656+zhamo026@users.noreply.github.com>
2025-04-05 07:07:08 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Website Loaded Successfully!");
+  console.log("Website Loaded Successfully!");
+
+  const toggle = document.getElementById("projects-toggle");
+  const menu = document.getElementById("projects-menu");
+
+  toggle.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu.classList.toggle("show");
+  });
 });
